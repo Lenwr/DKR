797d2a3360cc473eebab4c3fb34e336cb77a3f08
Lenwr <saidabou.lateam@gmail.com>
2024-08-29 15:22:37 +0200

    images

--- src/components/menuCard.vue
+++ src/components/menuCard.vue
@@ -14,7 +14,7 @@ defineProps({
   <div class="p-2  ">
     <div class="flex  flex-col rounded-xl  opacity-1 bg-white ">
       <div class="">
-        <img class="w-full rounded-xl h-auto" src="https://img.freepik.com/photos-gratuite/cuisine-asiatique-dans-restaurant_7939-2024.jpg?t=st=1724746632~exp=1724750232~hmac=34e8eba5aff8279b2ed4e77b515692a771ee2be4d4883711571c258a7b56d8fa&w=1800" alt="">
+        <img class="w-full rounded-xl h-auto" :src="picture" alt="">
       </div>
       <div class=" flex flex-col items-center justify-between p-2">
         <h1 class="font-semibold my-2 ">{{(name).toUpperCase()}}</h1>
--- src/constants.js
+++ src/constants.js
@@ -90,35 +90,35 @@ export const foodList = [
         name: 'Piment' ,
         description :'',
         price: 0.50,
-        picture: '/vite.svg'
+        picture: 'https://www.asiamarche.fr/cdn/shop/files/sauce-pimentee-sriracha-150g-coq_460x@2x.jpg?v=1702394744'
     },
     {
         category:'supply',
         name: 'Sauce spéciale oignon' ,
         description :'',
         price: 0.50,
-        picture: '/vite.svg'
+        picture: 'https://i.ytimg.com/vi/AounyXKpYAA/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDPPCp6rbP7qhYOBwjfaRjEeM-4eQ'
     },
     {
         category:'supply',
         name: 'sauce pastel' ,
         description :'',
         price: 0.50,
-        picture: '/vite.svg'
+        picture: 'https://aistoucuisine.com/wp-content/uploads/2016/05/pastelslivre18-scaled.jpg'
     },
     {
         category:'supply',
         name: 'mayonnaise' ,
         description :'',
         price: 0.50,
-        picture: '/vite.svg'
+        picture: 'https://sweetstore.fr/410-large_default/sauce-mayonnaise-500ml.jpg'
     },
     {
         category:'supply',
         name: 'sauce verte' ,
         description :'',
         price: 0.50,
-        picture: '/vite.svg'
+        picture: 'https://www.academiedugout.fr/images/75993/948-580/adobestock_170773372.jpeg?poix=50&poiy=50'
     },
 
     {
@@ -127,7 +127,7 @@ export const foodList = [
         type:'',
         description :'3 pilons de poulet + 1 accompagnement + 1 Boisson 33cL',
         price: 6.50,
-        picture: '/vite.svg'
+        picture: 'https://images.radio-canada.ca/v1/alimentation/recette/1x1/pilons-poulet-tandoori.jpg'
     },
     {
         category:'menu',
@@ -135,7 +135,7 @@ export const foodList = [
         type:'',
         description :'4 ailes de poulet + 1 accompagnements + 1 boisson 33 cL',
         price: 6.50,
-        picture: '/vite.svg'
+        picture: 'https://assets.afcdn.com/recipe/20201231/116900_w1024h1024c1cx968cy774cxt0cyt0cxb1936cyb1549.webp'
     },
     {
         category:'menu',
@@ -143,7 +143,7 @@ export const foodList = [
         type:'',
         description :'1 cuisse de poulet + 1 accompagnement + 1 boisson 33 cL',
         price: 6.50,
-        picture: '/vite.svg'
+        picture: 'https://cdn.pratico-pratiques.com/app/uploads/sites/2/2019/06/20083848/cuisses-de-poulet-a-la-portugaise.jpg'
     },
 
     {
@@ -151,56 +151,56 @@ export const foodList = [
         name: 'tiep viande' ,
         description :'',
         price: 12.00,
-        picture: '/tieppoisson.jpeg'
+        picture: 'https://www.tabouencuisine.com/wp-content/uploads/2017/11/ob_cf05fb_fotor-151006393385092-1.jpg'
     },
     {
         category:'plats',
         name: 'tiep poulet' ,
         description :'',
         price: 11.00,
-        picture: '/vite.svg'
+        picture: 'https://twicpics.moulinex.fr/https://sebplatform.api.groupe-seb.com/statics/85f6ed07-1f89-4c73-9204-bfe9ba610224.jpg?w=1920&fit=scale'
     },
     {
         category:'plats',
         name: 'thieboudienne' ,
         description :' ',
         price: 11.00,
-        picture: '/vite.svg'
+        picture: 'https://img.lemde.fr/2018/03/27/0/0/5760/3840/1440/960/60/0/532f98c_18068-6hi00q.b16kl.jpg'
     },
     {
         category:'plats',
         name: 'Yassa poulet' ,
         description :'',
         price: 11.00,
-        picture: '/vite.svg'
+        picture: 'https://chefrecettes.s3.eu-west-3.amazonaws.com/recettes/605c8cb4e5c85baa1876355f01bc86ebccac44755304e.jpeg'
     },
     {
         category:'plats',
         name: 'Mafé viande' ,
         description :'',
         price: 12.00,
-        picture: '/vite.svg'
+        picture: '/mafeviande.jpeg'
     },
     {
         category:'plats',
         name: 'attiéké poulet' ,
         description :'',
         price: 11.00,
-        picture: '/vite.svg'
+        picture: 'https://info.lenord.fr/upload/images/ca_cdj_05022052_web-5e5cc1f064063.jpeg'
     },
     {
         category:'plats',
         name: 'attiéké poissons' ,
         description :'',
         price: 11.00,
-        picture: '/vite.svg'
+        picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1SGTTaxSuwDrklSycs-Lm7znIr6LeIaWHBA&s'
     },
     {
         category:'plats',
         name: 'Vermicelles poulet' ,
         description :'',
         price: 10.00,
-        picture: '/vite.svg'
+        picture: 'https://i.goopics.net/a3yx4t.png'
     },
 
     {
@@ -208,49 +208,48 @@ export const foodList = [
         name: 'Jus de bissap' ,
         description :'',
         price: 2.50,
-        picture: '/vite.svg'
+        picture: 'https://www.restaurantorchideeplus.com/wp-content/uploads/2023/11/bissap.jpeg'
     },
     {
         category:'boissons',
         name: 'Jus de gingembre' ,
         description :'',
         price: 2.50,
-        picture: '/vite.svg'
+        picture: 'https://www.monsieurjus.com/assets/img/jus-gingembre_86127fac197e77d0e00d3ceb6a2bb86d.jpg'
     },
     {
         category:'boissons',
         name: 'Jus de bouyi' ,
         description :'',
         price: 2.50,
-        picture: '/vite.svg'
+        picture: 'https://www.labonnecuisine.be/wp-content/uploads/2016/02/indexk.jpeg'
     },
     {
         category:'boissons',
         name: 'Thiakry / dégué' ,
         description :'',
         price: 3.00,
-        picture: '/vite.svg'
+        picture: 'https://assets.afcdn.com/recipe/20210304/118344_w1024h1024c1cx1060cy707cxt0cyt0cxb2121cyb1414.jpg'
     },
     {
         category:'boissons',
         name: 'Canette' ,
         description :'',
         price: 1.00,
-        picture: '/vite.svg'
-    },
+        picture: 'https://versaillescommerces.fr/media/cache/wishibam_shop_product_thumbnail/66/b4/fcc21269515e9b59799eae536aa4.png'  },
     {
         category:'boissons',
         name: 'Eau' ,
         description :'',
         price: 1.00,
-        picture: '/vite.svg'
+        picture: 'https://www.piperpub.fr/wp-content/uploads/2021/03/bouteille-eau-50.jpg'
     },
     {
         category:'boissons',
         name: 'Bouteille' ,
         description :'1L5',
         price: 2.50,
-        picture: '/vite.svg'
+        picture: 'https://mon-plateau-repas.com/103-medium_default/bouteille-d-eau-plate-50cl.jpg'
     },
 
     {
@@ -258,7 +257,7 @@ export const foodList = [
         name: 'Poulet entier ',
         description: '',
         price: 7.00,
-        picture: 'https://epicier.ca/wp-content/uploads/2021/04/cuisson-poulet-BBQ-e1698957923158.jpg'
+        picture: 'https://poulet-a-la-braise-1996.fr/uploads/media/images/cms/medias/thumb_/cms/medias/6594062a492b7_images_large.jpeg'
     },
     {
         category: 'chicken',
@@ -272,35 +271,35 @@ export const foodList = [
         name: 'Cuisse de poulet  ' ,
         description :'',
         price: 2.50,
-        picture: 'https://images.ricardocuisine.com/services/recipes/992x1340_5459.jpg'
+        picture: 'https://static.takeaway.com/images/restaurants/fr/R7N7O3R1/products/6449093_lamaisondupoulet_food_demipouletbraise.png'
     },
     {
         category:'chicken',
         name: 'Blanc de poulet  ' ,
         description :'',
         price: 3.50,
-        picture: '/vite.svg'
+        picture: 'https://www.cuisineculinaire.com/wp-content/uploads/2021/01/Blancs-de-poulet-au-four-recette-facile-rapide_1_680.jpg'
     },
     {
         category:'chicken',
         name: '3 pilons ' ,
         description :'',
         price: 2.50,
-        picture: '/vite.svg'
+        picture: 'https://gourmandiseassia.fr/wp-content/uploads/2019/02/PicsArt_03-04-12.30.04.jpg'
     },
     {
         category:'chicken',
         name: 'Saucisse de poulet' ,
         description :'',
         price: 1.50,
-        picture: '/vite.svg'
+        picture: 'https://www.reghalal.com/app/uploads/2021/02/SAUCISSES-VOLAILLE-r3-HD_r.jpg'
     },
     {
         category:'chicken',
         name: 'Donuts poulet' ,
         description :'',
         price: 2.00,
-        picture: '/vite.svg'
+        picture: 'https://www.volys.be/-/media/Volys-Tenant/Global-Site/Product-Cards/30415.ashx?h=700&w=960&la=fr&hash=3CC254987381C429B754BB0B6B2A8785'
     },
 
     {
@@ -308,21 +307,21 @@ export const foodList = [
         name: 'Brochettes de poulet' ,
         description :'1 pièce',
         price: 1.50,
-        picture: 'https://img.over-blog-kiwi.com/0/89/69/26/20180923/ob_71aa50_brochettes-de-poulet-marine-a-la-tom.JPG'
+        picture: 'https://lamarmitedecaro.fr/wp-content/uploads/2023/11/Brochettes-poulet-marine.jpg'
     },
     {
         category:'grillades',
         name: 'Brochettes de boeuf' ,
         description :'1 pièce',
         price: 2.00,
-        picture: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.academiedugout.fr%2Frecettes%2Fbrochettes-de-boeuf_6589_4&psig=AOvVaw3lD-iTZ33eZBcXRHO3KGNi&ust=1724916927696000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJDL976Wl4gDFQAAAAAdAAAAABAE'
+        picture: 'https://cdn.pratico-pratiques.com/app/uploads/sites/4/2018/08/30210658/brochettes-de-boeuf-caramelises.jpeg'
     },
     {
-        category:'grillades',
-        name: 'Dibi agneau 300g' ,
-        description :'',
+        category: 'grillades',
+        name: 'Dibi agneau 300g',
+        description: '',
         price: 10.00,
-        picture: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Faistoucuisine.com%2Framadan%2Fdibi-maison-au-four%2F19%2F04%2F2020%2F&psig=AOvVaw0fRrqmGuUgU5xfCVxTk24n&ust=1724916981290000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjTvdOWl4gDFQAAAAAdAAAAABAE'
-    },
+        picture: 'https://aistoucuisine.com/wp-content/uploads/2020/04/dibi1.jpg'
+    }
 
 ]
