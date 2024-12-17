c6eb008669494cdc036593128974bc48bae3e4bd
Lenwr <saidabou.lateam@gmail.com>
2024-12-17 16:29:36 +0100

    new design

--- src/constants.js
+++ src/constants.js
@@ -31,56 +31,56 @@ export const foodList = [
     {
         category:'accompagnements',
         name: 'frites ' ,
-        description :'1',
+        description :'1 portion',
         price: 2.50,
         picture: 'https://img.cuisineaz.com/680x357/2015/09/30/i40123-10-choses-a-faire-avec-des-restes-de-frites.jpg'
     },
     {
         category:'accompagnements',
         name: 'frites maisons ' ,
-        description :'1',
+        description :'1 portion',
         price: 3.00,
         picture: 'https://res.cloudinary.com/hv9ssmzrz/image/fetch/c_fill,f_auto,h_488,q_auto,dpr_2.0,w_650/https://s3-eu-west-1.amazonaws.com/images-ca-1-0-1-eu/recipe_photos/original/197334/ob_5c15f8_frites-maison.jpg'
     },
     {
         category:'accompagnements',
         name: 'riz parfumé blanc' ,
-        description :'1',
+        description :'1 portion',
         price: 2.50,
         picture: 'https://laliregal.fr/cdn/shop/products/image_cc7a5386-5053-405e-a5c1-2ac631ee64ea_1024x.jpg?v=1588237263'
     },
     {
         category:'accompagnements',
         name: 'Patate douce' ,
-        description :'1',
+        description :'1 portion',
         price: 4.00,
         picture: 'https://fr.frije.com/content/recipes/111962/800-1.jpg'
     },
     {
         category:'accompagnements',
         name: 'aloko' ,
-        description :'1',
+        description :'1 portion',
         price: 4.00,
         picture: 'https://www.naboupastel.com/wp-content/uploads/2023/10/Aloko.jpg'
     },
     {
         category:'accompagnements',
         name: 'attiéké' ,
-        description :'1',
+        description :'1 portion',
         price: 4.00,
         picture: 'https://upload.wikimedia.org/wikipedia/commons/8/8d/Plat_atti%C3%A9k%C3%A9.jpg'
     },
     {
         category:'accompagnements',
         name: 'Riz thaï' ,
-        description :'1',
+        description :'1 portion',
         price: 3.00,
         picture: 'https://www.saveursdumonde.net/wp-content/uploads/2018/07/recette-de-riz-thai-e1532266320222.jpeg'
     },
     {
         category:'accompagnements',
         name: 'Pomme de terre' ,
-        description :'1',
+        description :'1 portion',
         price: 3.00,
         picture: 'https://platetrecette.com/wp-content/uploads/2020/05/Pommes-de-Terre-au-Four-WW.png'
     },
@@ -149,63 +149,63 @@ export const foodList = [
     {
         category:'plats',
         name: 'tiep viande' ,
-        description :'1',
+        description :'1 plat',
         price: 12.00,
         picture: 'https://www.tabouencuisine.com/wp-content/uploads/2017/11/ob_cf05fb_fotor-151006393385092-1.jpg'
     },
     {
         category:'plats',
         name: 'tiep poulet' ,
-        description :'1',
+        description :'1 plat',
         price: 11.00,
         picture: 'https://twicpics.moulinex.fr/https://sebplatform.api.groupe-seb.com/statics/85f6ed07-1f89-4c73-9204-bfe9ba610224.jpg?w=1920&fit=scale'
     },
     {
         category:'plats',
         name: 'thieboudienne' ,
-        description :' 1',
+        description :' 1 plat',
         price: 11.00,
         picture: 'https://img.lemde.fr/2018/03/27/0/0/5760/3840/1440/960/60/0/532f98c_18068-6hi00q.b16kl.jpg'
     },
     {
         category:'plats',
         name: 'Yassa poulet' ,
-        description :'1',
+        description :'1 plat',
         price: 11.00,
         picture: 'https://chefrecettes.s3.eu-west-3.amazonaws.com/recettes/605c8cb4e5c85baa1876355f01bc86ebccac44755304e.jpeg'
     },
     {
         category:'plats',
         name: 'Mafé viande' ,
-        description :'1',
+        description :'1 plat',
         price: 12.00,
         picture: '/mafeviande.jpeg'
     },
     {
         category:'plats',
         name: 'attiéké poulet' ,
-        description :'1',
+        description :'1 plat',
         price: 11.00,
         picture: 'https://info.lenord.fr/upload/images/ca_cdj_05022052_web-5e5cc1f064063.jpeg'
     },
     {
         category:'plats',
         name: 'attiéké poissons' ,
-        description :'1',
+        description :'1 plat',
         price: 11.00,
         picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1SGTTaxSuwDrklSycs-Lm7znIr6LeIaWHBA&s'
     },
     {
         category:'plats',
         name: 'Vermicelles poulet' ,
-        description :'1',
+        description :'1 plat',
         price: 10.00,
         picture: 'https://i.goopics.net/a3yx4t.png'
     },
     {
         category:'plats',
         name: 'Pattes bolognaises' ,
-        description :'1',
+        description :'1 plat',
         price: 3.00,
         picture: 'https://cdn.pratico-pratiques.com/app/uploads/sites/4/2018/08/30171718/spaghettis-alla-bolognese.jpeg'
     },
@@ -213,7 +213,7 @@ export const foodList = [
     {
         category:'boissons',
         name: 'Jus de bissap' ,
-        description :'1',
+        description :'1 ',
         price: 2.50,
         picture: 'https://www.restaurantorchideeplus.com/wp-content/uploads/2023/11/bissap.jpeg'
     },
@@ -241,19 +241,19 @@ export const foodList = [
     {
         category:'boissons',
         name: 'Canette' ,
-        description :'1',
+        description :'1 canette',
         price: 1.00,
         picture: 'https://versaillescommerces.fr/media/cache/wishibam_shop_product_thumbnail/66/b4/fcc21269515e9b59799eae536aa4.png'  },
     {
         category:'boissons',
         name: 'Eau' ,
-        description :'1',
+        description :'1 bouteille',
         price: 1.00,
         picture: 'https://www.piperpub.fr/wp-content/uploads/2021/03/bouteille-eau-50.jpg'
     },
     {
         category:'boissons',
-        name: 'Bouteille' ,
+        name: 'Eau' ,
         description :'1L5',
         price: 2.50,
         picture: 'https://mon-plateau-repas.com/103-medium_default/bouteille-d-eau-plate-50cl.jpg'
@@ -262,56 +262,56 @@ export const foodList = [
     {
         category: 'chicken',
         name: 'Poulet entier ',
-        description: '1',
+        description: '1P',
         price: 7.00,
         picture: 'https://poulet-a-la-braise-1996.fr/uploads/media/images/cms/medias/thumb_/cms/medias/6594062a492b7_images_large.jpeg'
     },
     {
         category: 'chicken',
         name: '1/2 poulet  ',
-        description: '1',
+        description: '1P',
         price: 3.50,
         picture: 'https://glovo.dhmedia.io/image/menus-glovo/products/5da1b84fd465ff07221e11b00818bf6f42b0e6ae9c50b619380cd10ead623f9f?t=W3siYXZpZiI6eyJxIjoibG93In19LHsicmVzaXplIjp7IndpZHRoIjoyNjAsImhlaWdodCI6MjYwfX1d'
     },
     {
         category:'chicken',
         name: 'Cuisse de poulet  ' ,
-        description :'1',
+        description :'1P',
         price: 2.00,
         picture: 'https://static.takeaway.com/images/restaurants/fr/R7N7O3R1/products/6449093_lamaisondupoulet_food_demipouletbraise.png'
     },
     {
         category:'chicken',
         name: 'Blanc de poulet  ' ,
-        description :'1',
+        description :'1P',
         price: 3.50,
         picture: 'https://www.cuisineculinaire.com/wp-content/uploads/2021/01/Blancs-de-poulet-au-four-recette-facile-rapide_1_680.jpg'
     },
     {
         category:'chicken',
-        name: '3 pilons ' ,
-        description :'1',
+        name: 'Pilon ' ,
+        description :'3 pièces',
         price: 3.00,
         picture: 'https://gourmandiseassia.fr/wp-content/uploads/2019/02/PicsArt_03-04-12.30.04.jpg'
     },
     {
         category:'chicken',
-        name: '3 ailes de poulet ' ,
-        description :'1',
+        name: 'Ailes de poulet ' ,
+        description :'3 pièces',
         price: 2.00,
         picture: 'https://m1.quebecormedia.com/emp/cdp_prod/bf9e143d34af42d006e1b2329cc9e0b88c799be3/AdobeStock-166037353.jpg?impolicy=resize&width=1500&height=1500'
     },
     {
         category:'chicken',
         name: 'Saucisse de poulet' ,
-        description :'1',
+        description :'1P',
         price: 1.50,
         picture: 'https://www.reghalal.com/app/uploads/2021/02/SAUCISSES-VOLAILLE-r3-HD_r.jpg'
     },
     {
         category:'chicken',
         name: 'Donuts poulet' ,
-        description :'1',
+        description :'1P',
         price: 2.00,
         picture: 'https://www.volys.be/-/media/Volys-Tenant/Global-Site/Product-Cards/30415.ashx?h=700&w=960&la=fr&hash=3CC254987381C429B754BB0B6B2A8785'
     },
@@ -319,21 +319,21 @@ export const foodList = [
     {
         category:'grillades',
         name: 'Brochettes de poulet' ,
-        description :'1 pièce',
+        description :'1 plat',
         price: 1.50,
         picture: 'https://lamarmitedecaro.fr/wp-content/uploads/2023/11/Brochettes-poulet-marine.jpg'
     },
     {
         category:'grillades',
         name: 'Brochettes de boeuf' ,
-        description :'1 pièce',
+        description :'1 plat',
         price: 2.00,
         picture: 'https://cdn.pratico-pratiques.com/app/uploads/sites/4/2018/08/30210658/brochettes-de-boeuf-caramelises.jpeg'
     },
     {
         category: 'grillades',
         name: 'Dibi agneau 300g',
-        description: '1',
+        description: '1 plat',
         price: 10.00,
         picture: 'https://aistoucuisine.com/wp-content/uploads/2020/04/dibi1.jpg'
     }
